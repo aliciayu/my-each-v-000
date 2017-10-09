8439efd32c5cce9012bb9daf4656cd70e11b2b8d
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-09 21:50:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(words) # put argument(s) here # code here
-    index = 0
-    while index < words.length
-      yield(words[index])
-      index += 1
-    end
-    words
+  index = 0
+  while index < words.length
+    yield(words[index])
+    index += 1
   end
+  words
 end
